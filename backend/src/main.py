f940bd03cf030fe312a2b346927c53fe184a7f31
Chien Nguyen <chienguyen97@gmail.com>
2023-06-17 23:44:20 +0700

    chore: customize prompt

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,7 +5,13 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import SupabaseVectorStore
 from langchain.document_loaders import TextLoader
 from langchain.llms import OpenAI
-from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import (
+    ConversationalRetrievalChain,
+    LLMChain
+)
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
 
 
 supabase_url = os.environ.get("SUPABASE_URL")
@@ -38,10 +44,66 @@ vector_store = SupabaseVectorStore(client=supabase, embedding=embeddings, table_
 # print(matched_docs[0].page_content)
 
 # -------- 3. Chatbot
-qa = ConversationalRetrievalChain.from_llm(OpenAI(temperature=0), vector_store.as_retriever())
-result = qa({"question": "How long does it take to deliver", "chat_history": []})
+template = """Given the following chat history and a follow up question, rephrase the follow up input question to be a standalone question.
+Or end the conversation if it seems like it's done.
+Chat History:\"""
+{chat_history}
+\"""
+Follow Up Input: \"""
+{question}
+\"""
+Standalone question:"""
+ 
+condense_question_prompt = PromptTemplate.from_template(template)
+ 
+template = """You are a friendly, conversational retail shopping assistant. Use the following context including product names, descriptions, and keywords to show the shopper whats available, help find what they want, and answer any questions.
+ 
+It's ok if you don't know the answer.
+Context:\"""
+ 
+{context}
+\"""
+Question:\"
+\"""
+ 
+Helpful Answer:"""
+ 
+qa_prompt= PromptTemplate.from_template(template)
+
+llm = OpenAI(temperature=0)
+
+# use the LLM Chain to create a question creation chain
+streaming_llm = OpenAI(
+    streaming=True,
+    verbose=True,
+    max_tokens=150,
+    temperature=0.2
+)
+
+question_generator = LLMChain(
+    llm=llm,
+    prompt=condense_question_prompt
+)
+ 
+# use the streaming LLM to create a question answering chain
+doc_chain = load_qa_chain(
+    llm=streaming_llm,
+    chain_type="stuff",
+    prompt=qa_prompt
+)
+
+chat_history = []
+qa = ConversationalRetrievalChain(
+        retriever=vector_store.as_retriever(),
+        combine_docs_chain=doc_chain,
+        question_generator=question_generator
+        )
+
+result = qa({"question": "How long does it take to deliver", "chat_history": chat_history})
 
 print('Question: ', result['question'])
 print('Answer: ', result['answer'])
+
+
 
 
